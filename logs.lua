475a9a743e4a6a1eb79fe668cdb9d979264bd211
阿刀 <code@aknife.cn>
2020-11-21 17:55:19 +0800

    '错误修正'

--- logs.lua
+++ logs.lua
@@ -26,27 +26,27 @@ if waf_logs ~= nil or type(waf_logs) == "table" then
         f:write(logs)
         f:close()
     end
-end
+else
+    -- 访问日志,未拦截的时候才记录
+    local acc_logs = ngx.ctx.acc
+    if acc_logs ~= nil or type(acc_logs) == "table" then
+        -- 创建目录
+        local acc_path = ngx.ctx.acc_path .. acc_logs["host"] .. "/"
+        if os.execute("cd "..acc_path) ~= 0 then
+            os.execute("mkdir -p " .. acc_path)
+            os.execute("chmod -R 0777 " .. acc_path)
+        end
 
--- 访问日志,未拦截的时候才记录
-local acc_logs = ngx.ctx.acc
-if waf_logs == nil acc_logs ~= nil or type(acc_logs) == "table" then
-    -- 创建目录
-    local acc_path = ngx.ctx.acc_path .. acc_logs["host"] .. "/"
-    if os.execute("cd "..acc_path) ~= 0 then
-        os.execute("mkdir -p " .. acc_path)
-        os.execute("chmod -R 0777 " .. acc_path)
+        -- 组合日志记录
+        local logs = ""
+        if ngx.ctx.type == "json" then
+            logs = cjson.encode(acc_logs) .. "\n\n"
+        else
+            logs =  "请求域名：" .. acc_logs["host"] .."\n请求时间：" .. acc_logs["time"] .."\n响应代码：" .. waf_logs["status"] .."\n客户端IP：" .. acc_logs["client"] .."\nRemote-IP：" .. acc_logs["remote"] .. "\n请求参数：" .. acc_logs["uri"] .. "\n请求方法：" .. acc_logs["method"] .. "\nPOST参数:"..acc_logs["post"] .. "\nUser-Agent：" .. acc_logs["agent"] .. "\n\n"
+        end
+
+        local f = assert(io.open(acc_path..dayDate..".log", "a"))
+        f:write(logs)
+        f:close()
     end
-
-    -- 组合日志记录
-    local logs = ""
-    if ngx.ctx.type == "json" then
-        logs = cjson.encode(acc_logs) .. "\n\n"
-    else
-        logs =  "请求域名：" .. acc_logs["host"] .."\n请求时间：" .. acc_logs["time"] .."\n响应代码：" .. waf_logs["status"] .."\n客户端IP：" .. acc_logs["client"] .."\nRemote-IP：" .. acc_logs["remote"] .. "\n请求参数：" .. acc_logs["uri"] .. "\n请求方法：" .. acc_logs["method"] .. "\nPOST参数:"..acc_logs["post"] .. "\nUser-Agent：" .. acc_logs["agent"] .. "\n\n"
-    end
-
-    local f = assert(io.open(acc_path..dayDate..".log", "a"))
-    f:write(logs)
-    f:close()
 end
